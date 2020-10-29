9aca908a07f94adc5140a87156283e0c63fa2fcd
Annalise Murphy <nali247@gmail.com>
2020-10-28 22:50:59 -0700

    to pass CI

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,13 +6,11 @@ import {
 } from 'react-router-dom';
 import CharacterList from '../characters/CharacterList.jsx';
 import Header from '../header/Header.jsx';
-import { useThemePicker } from '../../hooks/theme.js';
+
 
 export default function App() {
-  // const { themeMode } = useThemePicker();
-
+  
   return (
-    // eslint-disable-next-line max-len
     <Router>
       <Header />
       <Switch>
